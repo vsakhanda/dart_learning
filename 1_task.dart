4136b92ed88b691db110f3d33c05918804675038
Odyssey <59223551+vsakhanda@users.noreply.github.com>
2025-06-30 15:07:54 +0300

    feat(DART-06): first task mathematical operators

--- 1_task.dart
+++ 1_task.dart
@@ -1,6 +1,23 @@
-void main () {
-  print("feat(DART-06): initial commit") ;
-  print("second raw") ;
+void main() {
+  print("feat(DART-06): initial commit");
+  print("second raw");
+  print("*********************");
+  firstTask();
 }
 
+void firstTask() {
+   print("**Task one**");
+  double length = 18.4;
+  double heigth = 30.9;
 
+  var area = length * heigth;
+  var perimeter = 2 * (length + heigth);
+
+  print(
+    "If lenght is $length milimeters and height is $heigth milimeters. \n Area of the rectangle will be equel to $area.  \nPerimeter of the rectangle wiil be $perimeter",
+  );
+
+  var proximateSqure = area ~/ 1;
+  print("*********************");
+  print("Approximate area of the rectangle is $proximateSqure ");
+}
