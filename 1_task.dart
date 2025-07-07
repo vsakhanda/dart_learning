8e3461d60679d13179d077d632ee4875ede9e37d
Odyssey <59223551+vsakhanda@users.noreply.github.com>
2025-07-07 17:17:31 +0300

    feat(DART-06): changes according to Code review

--- 1_task.dart
+++ 1_task.dart
@@ -15,30 +15,30 @@ void main() {
 }
 
 void firstTask() {
-  print("**Task one: Mathematical operators**");
-  double length = 18.4;
-  double heigth = 30.9;
+  print('**Task one: Mathematical operators**');
+  final double length = 18.4;
+  final double heigth = 30.9;
 
   var area = length * heigth;
   var perimeter = 2 * (length + heigth);
 
   print(
-    "If lenght is $length milimeters and height is $heigth milimeters. \n Area of the rectangle will be equel to $area.  \nPerimeter of the rectangle wiil be $perimeter",
+    'If lenght is $length milimeters and height is $heigth milimeters. \n Area of the rectangle will be equel to $area.  \nPerimeter of the rectangle wiil be $perimeter',
   );
 
   var proximateSqure = area ~/ 1;
 
-  print("Approximate area of the rectangle is $proximateSqure ");
-  print("*********************");
+  print('Approximate area of the rectangle is $proximateSqure ');
+  print('*********************');
 }
 
 void secondTask() {
-  print("**Task two: Comparison operators**");
+  print('**Task two: Comparison operators**');
 
-  double number = 32;
-  var cash = 50;
-  var deposit = 100;
-  double divisor = 8;
+  final double number = 32;
+  final cash = 50;
+  final deposit = 100;
+  final double divisor = 8;
 
   print('number == cash: ${number >= cash}');
   print('number == deposit: ${number <= deposit}');
@@ -52,39 +52,39 @@ void secondTask() {
 
   // print('$devide');
   // print('number / $divisor: ${devide == 0}');
-  print("*********************");
+  print('*********************');
 }
 
 void thirdTask() {
-  print("**Task three: Logical operators**");
+  print('**Task three: Logical operators**');
 
-  bool hasMoney = true;
-  bool isOpen = false;
+  final bool hasMoney = true;
+  final bool isOpen = false;
 
-  bool canIBuy = (hasMoney && isOpen);
-  bool shouldIWait = hasMoney && !isOpen;
+  final bool canIBuy = (hasMoney && isOpen);
+  final bool shouldIWait = hasMoney && !isOpen;
 
   print(
-    "Чи можна зробити покупку (якщо є гроші та магазин відкритий)? $canIBuy",
+    'Чи можна зробити покупку (якщо є гроші та магазин відкритий)? $canIBuy',
   );
   print(
-    "Чи потрібно почекати (якщо магазин закритий абонемає грошей)? $shouldIWait",
+    'Чи потрібно почекати (якщо магазин закритий абонемає грошей)? $shouldIWait',
   );
 
   print("*********************");
 }
 
 void fourthTask() {
-  print("**Task four: Ternary Operators**");
+  print('**Task four: Ternary Operators**');
 
-  var temperature = 27;
+  final temperature = 27;
 
   print('Сьогодні температура на вулиці $temperature градусів за цельсієм');
   // String mood ;
   if (temperature > 25) {
-    print("Відчувається Тепло");
+    print('Відчувається Тепло');
   } else {
-    print("Відчувається Прохолодно");
+    print('Відчувається Прохолодно');
   }
   ;
 
@@ -98,12 +98,12 @@ void fourthTask() {
 }
 
 void fifthTask() {
-  print("**Task Five: Assignment Operators**");
+  print('**Task Five: Assignment Operators**');
 
   double score = 0;
-  double correctAnswers = 17;
-  double mistakes = 3;
-  double totalQuestions = 20;
+  final double correctAnswers = 17;
+  final double mistakes = 3;
+  final double totalQuestions = 20;
 
   score += correctAnswers * 10;
   score -= mistakes * 5;
@@ -111,11 +111,11 @@ void fifthTask() {
   score /= totalQuestions;
 
   print('Загальна оцінка $score');
-  print("*********************");
+  print('*********************');
 }
 
 void sixthTask() {
-  print("**Task Six: IfElse Statements**");
+  print('**Task Six: IfElse Statements**');
 
   var examScore = 19;
 
@@ -131,11 +131,11 @@ void sixthTask() {
     print('Повторний курс');
   }
 
-  print("*********************");
+  print('*********************');
 }
 
 void seventhTask() {
-  print("**Task Seven: Switch  Statements**");
+  print('**Task Seven: Switch  Statements**');
 
   var dayNumber = 7;
 
@@ -147,7 +147,7 @@ void seventhTask() {
     case 3:
       print('Середа');
     case 4:
-      print("Четвер");
+      print('Четвер');
     case 5:
       print("П'ятниця");
     case 6:
@@ -159,10 +159,10 @@ void seventhTask() {
   }
 
   print("*********************");
-}
+  }
 
 void eighthTask() {
-  print("**Task Eight: For loop**");
+  print('**Task Eight: For loop**');
 
   int total = 0;
 
@@ -170,7 +170,7 @@ void eighthTask() {
     print('Число: $i');
   }
 
-  print("** Умова в циклі **");
+  print('** Умова в циклі **');
   for (int i = 1; i <= 10; i++) {
     if (i % 2 == 0) {
       print('Число: $i що дільться на 2');
@@ -179,7 +179,7 @@ void eighthTask() {
     }
   }
 
-  print("** Умова в циклі з використанням continue **");
+  print('** Умова в циклі з використанням continue **');
   for (int i = 1; i <= 10; i++) {
     if (i % 2 != 0) {
       continue;
@@ -197,7 +197,7 @@ void eighthTask() {
 }
 
 void ninthTask() {
-  print("**Task Nine: while loop**");
+  print('**Task Nine: while loop**');
   int counter = 10;
   while (counter >= 1) {
     if (counter % 2 == 0) {
@@ -211,7 +211,7 @@ void ninthTask() {
 }
 
 void tenthTask() {
-  print("**Task ten: Break and Continue**");
+  print('**Task ten: Break and Continue**');
   
     for (int i = 0; i < 20; i++) {
       if (i % 3 == 0) {
