c768492088abfc3395aa78211f1fb90698a9fd29
Odyssey <59223551+vsakhanda@users.noreply.github.com>
2025-07-14 20:14:08 +0300

    feat(DART-11): Task three added

--- lib/hw11/11_task.dart
+++ lib/hw11/11_task.dart
@@ -2,12 +2,17 @@ import 'dart:async';
 
 Future<void> main() async {
   print("**Start of the program **");
+  print("Task 1: Асинхронне отримання імені");
   final name = await fetchName();
-  print("Мене звати $name");
+  print("Мене звати $name\n" );
 
+  print("Task 2: Асинхронне отримання вікуі");
   final ageStr = await fetchAge();
   final age = int.parse(ageStr);
   print("Мені $age ${ageSuffix(age)}\n");
+
+  print("Task 3: Послідовне виконання:");
+  await runSequential();
 
   print('** end of the program**');
 }
@@ -15,7 +20,6 @@ Future<void> main() async {
 // Task 1: Асинхронне отримання імені
 
 Future<String> fetchName() async {
-  print("**Start of the task 1 **");
   await Future.delayed(Duration(seconds: 2));
   return "Владислав";
 }
@@ -25,10 +29,25 @@ Future<String> fetchAge() async {
   await Future.delayed(Duration(milliseconds: 1500));
   return "21";
 }
+
 String ageSuffix(int age) {
-  print("**Start of the task 2 **");
   if (age % 10 == 1 && age % 100 != 11) return "рік";
   if ([2, 3, 4].contains(age % 10) && !(age % 100 >= 12 && age % 100 <= 14))
     return "роки";
   return "років";
+}
+
+// Task 3: Послідовне виконання Future
+Future<void> runSequential() async {
+  final stopwatch = Stopwatch()..start();
+
+  final name = await fetchName();
+  print("Мене звати $name");
+
+  final ageStr = await fetchAge();
+  final age = int.parse(ageStr);
+  print("Мені $age ${ageSuffix(age)}");
+
+  stopwatch.stop();
+  print("Час послідовного виконання: ${stopwatch.elapsedMilliseconds} мс\n");
 }
